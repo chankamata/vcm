390c36955c8857f032dd876288cbfc0c56ab5798
Christian Santos <it.christiansantos@gmail.com>
2024-09-04 18:00:13 +0800

    update 04/09/2024

---
+++ page-artist-management.php
@@ -0,0 +1,73 @@
+<?php /*Template Name: Artist Management */
+get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/stories-content.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/lp-layout.css">
+<section class="hero-banner l-_hpx">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="_content mb_60">
+                <h1 class="header_text"><span>Elevate Your Career With a </span>Leading Artist Management in the
+                    Philippines</h1>
+                <img src="<?php echo $asset_path; ?>/assets/img/artist-management/banner-2.png" alt=""
+                    class="w-100 d-lg-none">
+                <div class="text">
+                    <p>Here at VCM Celebrity Source, we are committed to guiding artists through their careers as a
+                        leading celebrity talent agency in the Philippines. We serve as the essential link between
+                        talented individuals and the vibrant world of show business.</p>
+                    <p class="m-0">Whether you're a singer, dancer, actor, or social media influencer, an artist
+                        management agency in the Philippines like VCM can significantly elevate your career. We
+                        specialize in launching and nurturing talent, ensuring sustained success in the competitive
+                        entertainment industry.</p>
+                </div>
+            </div>
+            <div class="image_content  d-none d-lg-block">
+                <img src="<?php echo $asset_path; ?>/assets/img/artist-management/banner.png" alt="" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="what-is l-_hpx">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="content">
+                <h2>
+                    <detail class="d-lg-none">What is
+                        <span>Artist Management</span>
+                    </detail>
+                    <img src="<?php echo $asset_path; ?>/assets/img/artist-management/what-is-dt.png"
+                        alt="What is Artist Management" class="w-100 d-none d-lg-block">
+                </h2>
+                <img src="<?php echo $asset_path; ?>/assets/img/artist-management/what-is.png"
+                    alt="What is Artist Management" class="w-100 d-lg-none">
+                <div class="text">
+                    <p>Having a management is having an ally in the entertainment industry. Similar to a celebrity
+                        talent agency, our role resembles that of a supportive friend who looks after your career as if
+                        it were their own. We handle everything from securing bookings and negotiating contracts to
+                        refining your public image and managing your social media presence. </p>
+                    <p class="m-0">Essentially, we take care of the behind-the-scenes details so that you can shine
+                        where it matters most: in your craft as a star.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="lp_about_content l-_hpx">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="d-flex">
+                <div class="_image">
+                    <img src="<?php echo $asset_path; ?>/assets/img/index/mariel.png" alt="Partner with Our Talents">
+                </div>
+                <div class="_content">
+                    <div class="text">
+                        <h3>Partner with Our Talents</h3>
+                        <p class="m-0">Elevate your next event with an unforgettable celebrity appearance. Browse our
+                            list of celebrities, and let VCM connect you with the perfect star to make your occasion
+                            extraordinary.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
